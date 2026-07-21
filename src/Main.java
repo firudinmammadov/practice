080076e6c524daf18528d7c3eec98981b9869069
fullstack2201 <firudinmmmdov688@gmail.com>
2026-07-21 04:46:46 +0400

    Variables

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+//        String name = "John";
+//        System.out.println(name);
+
+//        int myNum = 15;
+//        System.out.println(myNum);
+
+//        int myNum;
+//        myNum = 15;
+//        System.out.println(myNum);
+
+//        int myNum = 15;
+//        myNum = 20;
+//        System.out.println(myNum);
+
+//        final int myNum = 15;
+//        myNum = 20;
+//        System.out.println(myNum);
+
+//        char myLetter = 'D';
+//        System.out.println(myLetter);
+
+//        boolean myBool = true;
+//        System.out.println(myBool);
+
+//        String myText = "Hello";
+//        System.out.println(myText);
+    }
+}
